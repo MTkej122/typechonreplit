87d120363102dcdce111c5db8793270e689f4780
Sayafx <97787820+Sayafx@users.noreply.github.com>
2022-07-14 15:14:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ wget https://github.com/typecho/typecho/releases/download/v1.2.0/typecho.zip
 unzip typecho.zip
 rm -rf typecho.zip
 cd usr/themes
-wget https://github.com/wehaox/Typecho-Butterfly/releases/download/1.5.0/butterfly_Release1.5.0.zip
-unzip butterfly_Release1.5.0.zip
+wget https://github.com/wehaox/Typecho-Butterfly/releases/download/1.5.3/butterfly_Release1.5.3-fix.zip
+unzip butterfly_Release1.5.3-fix.zip
 #after download if you dont have reCAPTCHA plugin del file usr/themes/butterfly/functions.php line 1528-1529 
-rm -rf butterfly_Release1.5.0.zip
+rm -rf butterfly_Release1.5.3-fix.zip
 #when all down run run bottom
